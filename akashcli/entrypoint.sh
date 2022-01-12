bac7efd3aea73257f68438be7b0f338f92bf906c
mconstant <bobbaxter@protonmail.com>
2022-01-12 17:39:25 -0500

    akash cli now working

--- akashcli/entrypoint.sh
+++ akashcli/entrypoint.sh
@@ -2,4 +2,7 @@
 set -eE -o pipefail
 source /home/$AKASH_USER/.profile
 
-akash
+AKASH_KEY_NAME=tempWallet
+echo "Using temporary Akash Key and Wallet with name $AKASH_KEY_NAME"
+
+yes $TEMP_WALLET_PASSWORD | akash keys add $AKASH_KEY_NAME
